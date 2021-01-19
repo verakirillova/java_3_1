02273f7b5d6255ce0b79541b8934f2f52cab2d92
Vera Kirillova <venerafilatova@mail.ru>
2021-01-19 22:11:55 +0300

    add Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 120;
+        int miles = 20;
+        int bonus = price / miles;
+
+        System.out.println(bonus);
+
+    }
+}
